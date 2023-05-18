f98989820d7cdb0eed025493ba5ff10cdf9d6b58
unknown <mcmahanconner@gmail.com>
2023-05-18 17:18:20 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+<div className="app">
+  Hello, world
+</div>
+  )
+}
+
+export default App
