2b8b21a9136c534f62ee584e70b950a360b90cba
unknown <mcmahanconner@gmail.com>
2023-05-18 18:05:39 -0400

    Testing things

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
-import React, { useState } from 'react'
-import './App.scss'
+import React, { useState } from "react";
+import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [hp, setHp] = useState(100);
+  const [inv, setInv] = useState({
+    knife:false,
+    flashlight: false
+  });
+
+
+  const handleChange = (event) => {
+    let value = parseInt(event.target.value);
+    switch (value) {
+      case 1:
+        setInv(prevInv => ({
+          ...prevInv,
+          knife: true
+        }))
+        break
+      default:
+        console.log("Error")
+        break
+    }
+  };
 
   return (
-<div className="app">
-  Hello, world
-</div>
-  )
+    <div className="app">
+      {inv.knife && "You have knife"}
+      Health: {hp}
+      <button onClick={handleChange} value={1}>
+        Pick up knife
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
