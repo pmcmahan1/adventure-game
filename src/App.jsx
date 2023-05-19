64d5e9abd73bae1624eecabef96532ca66bc1bee
unknown <mcmahanconner@gmail.com>
2023-05-19 02:52:43 -0400

    About finished

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import heart from "./assets/heart.png";
 import backpack from "./assets/backpack.png";
 import knife from "./assets/knife.png";
 import flashlight from "./assets/flashlight.png";
+import key from "./assets/key.png"
 import hallway from "./assets/hallway.jpg";
 import livingroom from "./assets/livingroom.jpg";
+import frontdoor from './assets/frontdoor.jpg'
 
 function App() {
   const [hp, setHp] = useState(100);
   const [inv, setInv] = useState({
     knife: false,
     flashlight: false,
+    key: false,
   });
   const [displayText, setDisplayText] = useState(
     "You awaken to find yourself in a dimly lit hallway."
@@ -33,7 +36,7 @@ function App() {
     let value = parseInt(event.target.value);
     switch (value) {
       case 1:
-        setDisplayText("You attempt to open the door, but the door is locked.");
+        setDisplayText("You attempt to open the door, but the door is jammed from the other side.");
         setValue1(99);
         break;
       case 2:
@@ -58,12 +61,12 @@ function App() {
         break;
       case 4:
         setDisplayText(
-          "You search the cupboard and find a knife and stow it away in your bag."
+          "You search the cupboard and find a key and stow it away in your pocket."
         );
         setValue1(99);
         setInv((prevInv) => ({
           ...prevInv,
-          knife: !prevInv.knife,
+          key: !prevInv.key,
         }));
         setChoiceThree("Continue through the building.");
         break;
@@ -77,10 +80,85 @@ function App() {
         setValue2(99)
         setChoiceThree("Continue through the building.");
         break;
+      case 6:
+        setBackground(frontdoor)
+        setDisplayText("You walk through the next doorway, and find what seems to be the entrance to the building.")
+        setValue1(8)
+        setValue2(9)
+        setValue3(10)
+        setValue4(11)
+        setChoiceOne("Try to open the door.")
+        setChoiceTwo("")
+        setChoiceThree("Study your surroundings.")
+        setChoiceFour("Turn around.")
+        break;
       case 7:
         setDisplayText("You stand in the middle of the room, pondering about how you came to be in this place.")
         setValue4(99)
         break;
+      case 8:
+        setDisplayText("You pull on the door but it is locked.")
+        setValue1(99)
+        if (inv.key) {
+          setChoiceTwo("Use your key on the locked door.")
+        }
+        break;
+      case 9:
+        setInv((prevInv) => ({
+          ...prevInv,
+          key: !prevInv.key,
+        }));
+        setDisplayText("Congratulations! You escaped the house. Game over.")
+        setChoiceOne("")
+        setChoiceTwo("")
+        setChoiceThree("")
+        setChoiceFour("")
+        break;
+      case 10:
+        setDisplayText("You study the room, however you see no options besides turning around or finding a way to open the locked door.")
+        setValue3(99)
+        break;
+        case 11:
+          setBackground(livingroom)
+          setDisplayText("You return to the living room.")
+          setChoiceOne("Search the cupboard.")
+          setChoiceTwo("Explore the room.")
+          setChoiceThree("Return to the front door.")
+          setChoiceFour("")
+          setValue1(12)
+          setValue2(13)
+          setValue3(14)
+          break;
+        case 12:
+          if (inv.key) {
+            setDisplayText("You search the cupboard once again and find nothing new.")
+            setValue1(99)
+            break
+          } else {
+            setDisplayText("You search the cupboard and find a key and stow it away in your pocket.")
+            setInv((prevInv) => ({
+              ...prevInv,
+              key: !prevInv.key,
+            }));
+            setValue1(99)
+            break
+          }
+        case 13:
+          setDisplayText("You find nothing worthwhile.")
+          setValue2(99)
+          break;
+        case 14:
+          setBackground(frontdoor)
+          setDisplayText("You return to the front door.")
+          setValue1(8)
+          setValue2(9)
+          setValue3(10)
+          setValue4(11)
+          setChoiceOne("Try to open the door.")
+          setChoiceTwo("")
+          setChoiceThree("Study your surroundings.")
+          setChoiceFour("Turn around.")
+          break;
       default:
         console.log("Error");
         break;
@@ -97,6 +175,7 @@ function App() {
         </div>
         <div className="inventory">
           <img src={backpack} alt="" />
+          {inv.key && <img src={key} alt="" className="inventory-item" />}
           {inv.knife && <img src={knife} alt="" className="inventory-item" />}
           {inv.flashlight && (
             <img src={flashlight} alt="" className="inventory-item" />
