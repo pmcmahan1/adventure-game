16ec47e6fac5dcd37a139e86b31f9eadd5b861d1
unknown <mcmahanconner@gmail.com>
2023-05-18 21:38:25 -0400

    Styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import heart from "./assets/heart.png";
 import backpack from "./assets/backpack.png";
 import knife from "./assets/knife.png";
 import flashlight from "./assets/flashlight.png";
+import hallway from './assets/hallway.jpg'
 
 function App() {
   const [hp, setHp] = useState(100);
@@ -48,6 +49,9 @@ function App() {
             <img src={flashlight} alt="" className="inventory-item" />
           )}
         </div>
+      </div>
+      <div className="image-display">
+        <img src={hallway} alt="" className="story-image" />
       </div>
       <div className="text-display">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt possimus sunt, autem quod deserunt quaerat, culpa sed fugit omnis, exercitationem veritatis itaque aliquid ab quo laborum recusandae excepturi eligendi quae!
