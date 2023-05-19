7978c7ad68d854db10b6961c968464386c10a508
unknown <mcmahanconner@gmail.com>
2023-05-18 20:55:16 -0400

    assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
 import React, { useState } from "react";
 import "./App.scss";
+import heart from "./assets/heart.png";
+import backpack from "./assets/backpack.png";
+import knife from "./assets/knife.png";
+import flashlight from "./assets/flashlight.png";
 
 function App() {
   const [hp, setHp] = useState(100);
   const [inv, setInv] = useState({
-    knife:false,
-    flashlight: false
+    knife: false,
+    flashlight: false,
   });
-
 
   const handleChange = (event) => {
     let value = parseInt(event.target.value);
     switch (value) {
       case 1:
-        setInv(prevInv => ({
+        setInv((prevInv) => ({
           ...prevInv,
-          knife: true
-        }))
-        break
+          knife: !prevInv.knife,
+        }));
+        break;
+      case 2:
+        setInv((prevInv) => ({
+          ...prevInv,
+          flashlight: !prevInv.flashlight,
+        }));
+        break;
       default:
-        console.log("Error")
-        break
+        console.log("Error");
+        break;
     }
   };
 
   return (
     <div className="app">
-      {inv.knife && "You have knife"}
-      Health: {hp}
+      <div className="status">
+        <div className="health">
+          <img src={heart} alt="" />
+          {hp}
+        </div>
+        <div className="inventory">
+          <img src={backpack} alt="" />
+          {inv.knife && <img src={knife} alt="" className="inventory-item" />}
+          {inv.flashlight && (
+            <img src={flashlight} alt="" className="inventory-item" />
+          )}
+        </div>
+      </div>
       <button onClick={handleChange} value={1}>
-        Pick up knife
+        knife
+      </button>
+      <button onClick={handleChange} value={2}>
+        flashlight
       </button>
     </div>
   );
